d3b80059d651c22f2fc7258ac98a08a04559c50d
Ethan Graf <graf.ethan@gmail.com>
2021-07-22 20:49:37 -0400

    Frontend makes basic http request to notes backend api

--- apps/notes/frontend/src/App.tsx
+++ apps/notes/frontend/src/App.tsx
@@ -1,24 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios, { CancelTokenSource } from 'axios';
 import './App.css';
+
+interface ListFilesResponse {
+  name: string
+  is_dir: boolean
+  filemode: number
+}
+
+const defaultFiles:ListFilesResponse[] = [];
+
+const FileList = () => {
+  const [files, setFiles]: [ListFilesResponse[], (files: ListFilesResponse[]) => void] = React.useState(
+    defaultFiles
+  );
+
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState<boolean>(true);
+
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ''
+  );
+
+  const cancelToken = axios.CancelToken; //create cancel token
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource,
+    (cancelTokenSource: CancelTokenSource) => void
+  ] = React.useState(cancelToken.source());
+
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('User cancelled operation');
+    }
+  };
+
+  React.useEffect(() => {
+    console.log("HELLO");
+    axios
+      .get<ListFilesResponse[]>('http://localhost:8000/ls', {
+        cancelToken: cancelTokenSource.token,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout: 10000,
+      })
+      .then((response) => {
+        console.log("WAUT");
+        console.log(response.data);
+        setFiles(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        console.log(ex);
+        if (ex.response) {
+          let error = axios.isCancel(ex)
+          ? 'Request Cancelled'
+          : ex.code === 'ECONNABORTED'
+          ? 'A timeout has occurred'
+          : ex.response.status === 404
+          ? 'Resource Not Found'
+          : 'An unexpected error has occurred';
+
+          setError(error);
+        }
+        setLoading(false);
+        console.log(ex.request);
+        setError("failed to load files");
+        });
+  }, []);
+
+  return (
+    <div className="FilesList">
+      {loading && <button onClick={handleCancelClick}>Cancel</button>}
+      <ul className="files">
+        {files.map((file) => (
+          <li key={file.name}>
+            <h3>{file.name}</h3>
+          </li>
+        ))}
+      </ul>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Welcome to Notes
+      <FileList></FileList>
     </div>
   );
 }
