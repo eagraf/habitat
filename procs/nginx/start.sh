7d388d74b315cb778a6d76c4f50fad896d522277
Ethan Graf <graf.ethan@gmail.com>
2021-08-25 01:37:46 -0400

    Remove individual app proc dirs

--- procs/nginx/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-export CONTENT_DIR=$SCRIPT_DIR/content
-
-docker stop habitat_nginx
-docker rm habitat_nginx
-docker run --rm -d -p 8081:80 -v $CONTENT_DIR:/usr/share/nginx/html --name habitat_nginx nginx
