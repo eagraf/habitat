adcaf095435aa6db821b0fb92edef1a6b8e5e27f
Ethan Graf <graf.ethan@gmail.com>
2021-08-25 01:30:33 -0400

    Process manager handles new centralized proc dir

--- cmd/habitat/procs/proc.go
+++ cmd/habitat/procs/proc.go
@@ -1,83 +1,53 @@
 package procs
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"syscall"
 
 	"github.com/rs/zerolog/log"
 )
 
 type Proc struct {
-	Name string
-	Path string
+	Name     string
+	CmdPath  string
+	DataPath string
 
 	cmd     *exec.Cmd
 	errChan chan ProcError
 }
 
-func NewProc(name, path string, errChan chan ProcError) *Proc {
+func NewProc(name, cmdPath, dataPath string, errChan chan ProcError) *Proc {
 	return &Proc{
-		Name: name,
-		Path: path,
+		Name:     name,
+		CmdPath:  cmdPath,
+		DataPath: dataPath,
 
 		errChan: errChan,
 	}
 }
 
 func (p *Proc) Start() error {
-	// make sure that proc dir exists
-	fileInfo, err := os.Stat(p.Path)
-	if err != nil {
-		return fmt.Errorf("couldn't find process dir %s", p.Path)
+	cmd := &exec.Cmd{
+		Path: p.CmdPath,
+		Env: []string{
+			fmt.Sprintf("DATA_DIR=%s", p.DataPath),
+		},
 	}
-	if !fileInfo.IsDir() {
-		return fmt.Errorf("%s is not a dir", p.Path)
-	}
-
-	// start process
-	startPath := filepath.Join(p.Path, "start.sh")
-	cmd := exec.Command(startPath)
 
 	// start this process with a groupd id equal to its pid. this allows for all of its subprocesses to be killed
 	// at once by passing in the negative pid to syscall.Kill
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	p.cmd = cmd
 
-	stdout, err := cmd.StdoutPipe()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Start()
 	if err != nil {
 		return err
 	}
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return err
-	}
-
-	scanner := bufio.NewScanner(stdout)
-	errScanner := bufio.NewScanner(stderr)
-
-	err = cmd.Start()
-	if err != nil {
-		return err
-	}
-
-	go func() {
-		for scanner.Scan() {
-			line := scanner.Text()
-			fmt.Println(line)
-		}
-	}()
-
-	go func() {
-		for errScanner.Scan() {
-			line := errScanner.Text()
-			fmt.Println(line)
-		}
-	}()
 
 	go func() {
 		err := cmd.Wait()
@@ -108,45 +78,6 @@ func (p *Proc) Stop() error {
 		}
 	}
 	defer terminateProcess(p.cmd.Process.Pid)
-
-	// if stop.sh script exists, execute it
-	stopPath := filepath.Join(p.Path, "stop.sh")
-	if _, err := os.Stat(stopPath); err == nil {
-		// stop process
-		cmd := exec.Command(stopPath)
-
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			return err
-		}
-
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			return err
-		}
-
-		scanner := bufio.NewScanner(stdout)
-		errScanner := bufio.NewScanner(stderr)
-
-		err = cmd.Start()
-		if err != nil {
-			return err
-		}
-
-		go func() {
-			for scanner.Scan() {
-				line := scanner.Text()
-				fmt.Println(line)
-			}
-		}()
-
-		go func() {
-			for errScanner.Scan() {
-				line := errScanner.Text()
-				fmt.Println(line)
-			}
-		}()
-	}
 
 	return nil
 }
