9eb6afddb4a3d0c98f7bf240d117a8d570f837b8
arushibandi <arb.arushibandi@gmail.com>
2021-09-16 09:57:47 -0400

    basic backend framework for communities app

--- apps/community/backend/cmd/api/main.go
+++ apps/community/backend/cmd/api/main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/eagraf/habitat/pkg/ipfs"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
-	"github.com/eagraf/habitat/pkg/ipfs"
 )
 
+//  bootstrap peers are expected to be "always online"
+//  does ipfs have a built in way to connect to all peers from the bootstrap?
+//  we might need some protocol to do this : get peers from bootstrap node and try to connect
 type CommunityConfig struct {
-	NodePath string
-	SwarmKey string
-	Address  string
+	Name           string
+	SwarmKey       string
+	BootstrapPeers []string // peer identities of nodes that are bootstrap
+	Peers          []string // peer identities of nodes that are just peers
 }
 
 func main() {
-	log.Info().Msg("starting notes api")
+	log.Info().Msg("starting communities api")
 
 	r := mux.NewRouter()
 
@@ -25,24 +32,41 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "0.0.0.0:8000",
+		Addr:         "0.0.0.0:8008",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Info().Msg("notes api listening on localhost:8000")
+	log.Info().Msg("communities api listening on localhost:8000")
 	log.Fatal().Err(srv.ListenAndServe())
 }
 
+// from https://github.com/Kubuxu/go-ipfs-swarm-key-gen/blob/master/ipfs-swarm-key-gen/main.go
 func KeyGen() string {
-	return ""
+	key := make([]byte, 32)
+	_, err := rand.Read(key)
+	if err != nil {
+		fmt.Println("While trying to read random source:", err)
+	}
+
+	// when writing to swarm.key, add these to the top:
+	// fmt.Println("/key/swarm/psk/1.0.0/")
+	// fmt.Println("/base16/")
+	return hex.EncodeToString(key)
 }
 
-// CreateCommunity: create a node with new peers, serves as default bootstrap for community
-// 1. Create Node
-// 2. Delete all peers
-// 3. Run daemon
-// 4. get swarm + address to broadcast
+/*
+ CreateCommunity: create a node with new peers, serves as default bootstrap for community
+ - Create Node
+ - Delete all peers
+ - Create swarm key and add to swarm.key
+ - Run daemon
+ - return swarm + address to broadcast
+ - this node automatically becomes the bootstrap peer (for now)
+ can either use the api returned by createNode or connect to a new client
+ TODO: 	create CommunityConfig struct which contains globals for the community like
+		swarm key and name of it and peer ids in it
+*/
 func CreateCommunity(name string, path string) (error, string, string) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -54,10 +78,33 @@ func CreateCommunity(name string, path string) (error, string, string) {
 	return nil, KeyGen(), api.identity
 }
 
-func JoinCommunity() {
+/*
+ JoinCommunity: the client doesn't have an IPFS node for this network yet:
+ - Create Node
+ - Delete all peers
+ - Add swarm to swarm.key
+ - Add bootstrap peers
+ - Run daemon
+ - Add regular peers
+ - Return peer id: need to kick off some way for all other nodes to add this node
+  can either use the api returned by createNode or connect to a new client
+*/
+func JoinCommunity(name string, path string, key string, btsppeers []string, peers []string) string {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	api, err := ipfs.createNode(ctx, path, true)
+	if err != nil {
+		return err, "", ""
+	}
 
+	return nil, api.identity
 }
 
+/*
+ ConnectCommunity:
+ - meant to be used by nodes that are already in a community
+ - just run the daemon & return the API or IPFS Client
+*/
 func ConnectCommunity() {
 
 }
