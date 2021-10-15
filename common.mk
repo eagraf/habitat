f0e94320c68eeaf6aee6990b0e64342b84e99fc3
Ethan Graf <graf.ethan@gmail.com>
2021-10-14 21:28:34 -0400

    Move apps.yml up a directory and put .gitignore in data/ as well
    - Fixed apps.yaml->apps.yml in .gitignore

--- common.mk
+++ common.mk
@@ -1,5 +1,7 @@
 MKFILE_PATH := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 export BIN_DIR := $(MKFILE_PATH)bin
-export DEV_PROC_DIR := $(MKFILE_PATH)procs
+export DEV_DATA_DIR := $(MKFILE_PATH)data
+export DEV_PROC_DIR := $(DEV_DATA_DIR)/procs
+export DEV_COMMUNITY_DIR := $(DEV_DATA_DIR)/communities
 export APPS_DIR := $(MKFILE_PATH)apps
