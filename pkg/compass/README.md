3ab9503104e3d137f32f3c24f9da2669d3154dbe
ethan <graf.ethan@gmail.com>
2021-12-24 00:49:38 -0500

    Created pkg/compass, which is a source of truth for important paths and addresses

---
+++ pkg/compass/README.md
@@ -0,0 +1,5 @@
+# Compass
+This library is the source of truth for important values like the locations of certain files, addresses for the current node, etc. It accounts for achitecture/operating system, local configuration and environment to produce these values.
+
+## TODO
+- Integrate with spf13/viper
---
+++ pkg/compass/helpers.go
@@ -0,0 +1,36 @@
+package compass
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+// readSingleValueConfigFile is useful for reading single value config files (e.g. /etc/hostname)
+// This function will panic under any error, as it assumes that it is opening critical config
+// that the program cannot run without
+func readSingleValueConfigFile(path string) string {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(fmt.Sprintf("unable to open %s: %s", path, err))
+	}
+	defer file.Close()
+
+	buf, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(fmt.Sprintf("unable to read %s: %s", path, err))
+	}
+
+	return string(buf)
+}
+
+// singleValueCommand is good for running commands that return a single value config (e.g. hostname)
+func singleValueCommand(cmd string) string {
+	command := exec.Command(cmd)
+	output, err := command.CombinedOutput()
+	if err != nil {
+		panic(fmt.Sprintf("error running %s: %s", cmd, err))
+	}
+	return string(output)
+}
