0e935dba872e2be9e4a1118e797ce34b3b4690bc
ethan <graf.ethan@gmail.com>
2022-03-31 00:06:11 -0400

    Base p2p impl look at this later

---
+++ cmd/habitat/p2p/p2p.go
@@ -0,0 +1,42 @@
+package p2p
+
+import (
+	"fmt"
+
+	"github.com/eagraf/habitat/pkg/compass"
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+const clientVersion = "cmd/habitat/p2p/0.0.1"
+
+type Node struct {
+	listenAddr ma.Multiaddr
+	host       host.Host
+}
+
+func NewNode(port string) *Node {
+	priv, _ := compass.GetPeerIDKeyPair()
+
+	ip, err := compass.LocalIPv4()
+	if err != nil {
+		panic(err)
+	}
+
+	listen, _ := ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%s", ip, port))
+	h, _ := libp2p.New(
+		libp2p.ListenAddrs(listen),
+		libp2p.Identity(priv),
+	)
+	node := &Node{
+		listenAddr: listen,
+		host:       h,
+	}
+
+	return node
+}
+
+func (n *Node) Host() host.Host {
+	return n.host
+}
