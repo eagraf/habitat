e7f8a0d51e9c9d1032eb01f51091e01bb0d87f6a
Ethan Graf <graf.ethan@gmail.com>
2022-04-17 18:29:05 -0400

    Begin implementing state system

---
+++ cmd/habitat/community/state/schema_test.go
@@ -0,0 +1,76 @@
+package state
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/qri-io/jsonschema"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommunityStateSchema(t *testing.T) {
+	rs := &jsonschema.Schema{}
+	err := json.Unmarshal(communityStateSchema, rs)
+	assert.Nil(t, err)
+
+	// Test that an empty CommunityState struct matches the schema
+	cs := CommunityState{}
+	marshaled, err := json.Marshal(&cs)
+	assert.Nil(t, err)
+	keyErrs, err := rs.ValidateBytes(context.Background(), marshaled)
+	assert.Nil(t, err)
+	if len(keyErrs) != 0 {
+		for _, e := range keyErrs {
+			t.Log(e)
+		}
+		t.Error()
+	}
+}
+
+func TestJSONState(t *testing.T) {
+	initState := []byte(`{
+		"community_id": "abc"
+	}`)
+	jsonState, keyErrs, err := NewJSONState(communityStateSchema, initState)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(keyErrs))
+
+	patch := []byte(`[{
+		"op": "add",
+		"path": "/counter",
+		"value": 1
+	}]`)
+
+	err = jsonState.ApplyPatch(patch)
+	assert.Nil(t, err)
+}
+
+func TestCounterIncrement(t *testing.T) {
+	jsonState, keyErrs, err := NewJSONState(communityStateSchema, []byte(`{"community_id":"abc"}`))
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(keyErrs))
+
+	csm := NewCommunityStateMachine(jsonState, &LocalDispatcher{jsonState: jsonState})
+	state, err := csm.State()
+	assert.Nil(t, err)
+	assert.Equal(t, state.CommunityID, "abc")
+
+	initCounter := &InitializeCounterTransition{
+		InitialCount: 1,
+	}
+	err = csm.ProposeTransition(initCounter)
+	assert.Nil(t, err)
+
+	state, err = csm.State()
+	assert.Nil(t, err)
+	assert.Equal(t, state.Counter, 1)
+
+	incCounter := &IncrementCounterTransition{}
+	err = csm.ProposeTransition(incCounter)
+	assert.Nil(t, err)
+
+	state, err = csm.State()
+	assert.Nil(t, err)
+	assert.Equal(t, state.Counter, 2)
+}
---
+++ cmd/habitat/community/state/state.go
@@ -0,0 +1,142 @@
+package state
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/qri-io/jsonschema"
+)
+
+func keyError(errs []jsonschema.KeyError) error {
+	s := strings.Builder{}
+	for _, e := range errs {
+		s.WriteString(fmt.Sprintf("%s\n", e.Error()))
+	}
+	return errors.New(s.String())
+}
+
+type CommunityStateMachine struct {
+	jsonState  *JSONState
+	dispatcher Dispatcher
+}
+
+func NewCommunityStateMachine(jsonState *JSONState, dispatcher Dispatcher) *CommunityStateMachine {
+	return &CommunityStateMachine{
+		jsonState:  jsonState,
+		dispatcher: dispatcher,
+	}
+}
+
+func (csm *CommunityStateMachine) ProposeTransition(t CommunityStateTransition) error {
+	currentState, err := csm.State()
+	if err != nil {
+		return err
+	}
+
+	err = t.Validate(currentState)
+	if err != nil {
+		return fmt.Errorf("transition validation failed: %s", err)
+	}
+
+	patch, err := t.Patch(currentState)
+	if err != nil {
+		return err
+	}
+
+	err = csm.jsonState.ValidatePatch(patch)
+	if err != nil {
+		return err
+	}
+
+	err = csm.dispatcher.Dispatch(patch)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (csm *CommunityStateMachine) State() (*CommunityState, error) {
+	var res CommunityState
+	err := csm.jsonState.Unmarshal(&res)
+	if err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+type JSONState struct {
+	schema *jsonschema.Schema
+	state  []byte
+
+	*sync.Mutex
+}
+
+func NewJSONState(jsonSchema []byte, initState []byte) (*JSONState, []jsonschema.KeyError, error) {
+	rs := &jsonschema.Schema{}
+	err := json.Unmarshal(jsonSchema, rs)
+	if err != nil {
+		return nil, nil, fmt.Errorf("invalid JSON schema: %s", err)
+	}
+	keyErrs, err := rs.ValidateBytes(context.Background(), initState)
+	if err != nil {
+		return nil, keyErrs, fmt.Errorf("error validating initial state")
+	}
+	if len(keyErrs) != 0 {
+		return nil, keyErrs, fmt.Errorf("invalid initial state")
+	}
+
+	return &JSONState{
+		schema: rs,
+		state:  initState,
+		Mutex:  &sync.Mutex{},
+	}, nil, nil
+}
+
+func (s *JSONState) ApplyPatch(patchJSON []byte) error {
+	updated, err := s.applyImpl(patchJSON)
+	if err != nil {
+		return err
+	}
+
+	// only update state if everything worked out
+	s.Lock()
+	defer s.Unlock()
+
+	s.state = updated
+
+	return nil
+}
+
+func (s *JSONState) ValidatePatch(patchJSON []byte) error {
+	_, err := s.applyImpl(patchJSON)
+	return err
+}
+
+func (s *JSONState) applyImpl(patchJSON []byte) ([]byte, error) {
+	patch, err := jsonpatch.DecodePatch(patchJSON)
+	if err != nil {
+		return nil, fmt.Errorf("invalid JSON patch: %s", err)
+	}
+	updated, err := patch.Apply(s.state)
+	if err != nil {
+		return nil, fmt.Errorf("error applying patch to current state")
+	}
+	// check that updated state still fulfills the schema
+	keyErrs, err := s.schema.ValidateBytes(context.Background(), updated)
+	if err != nil {
+		return nil, fmt.Errorf("error validating updated state: %s", err)
+	}
+	if len(keyErrs) != 0 {
+		return nil, keyError(keyErrs)
+	}
+	return updated, nil
+}
+
+func (s *JSONState) Unmarshal(dest interface{}) error {
+	return json.Unmarshal(s.state, dest)
+}
