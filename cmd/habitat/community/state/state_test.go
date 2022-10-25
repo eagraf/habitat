5e85731429a62ce407745351a573293f306aa00f
Ethan Graf <graf.ethan@gmail.com>
2022-10-25 15:12:44 -0400

    Add Nodes and Members to community state schema

--- cmd/habitat/community/state/state_test.go
+++ cmd/habitat/community/state/state_test.go
@@ -1,38 +1,14 @@
 package state
 
 import (
-	"context"
-	"encoding/json"
 	"testing"
 
 	"github.com/eagraf/habitat/structs/community"
-	"github.com/qri-io/jsonschema"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCommunityStateSchema(t *testing.T) {
-	rs := &jsonschema.Schema{}
-	err := json.Unmarshal(community.CommunityStateSchema, rs)
-	assert.Nil(t, err)
-
-	// Test that an empty CommunityState struct matches the schema
-	cs := community.CommunityState{}
-	marshaled, err := json.Marshal(&cs)
-	assert.Nil(t, err)
-	keyErrs, err := rs.ValidateBytes(context.Background(), marshaled)
-	assert.Nil(t, err)
-	if len(keyErrs) != 0 {
-		for _, e := range keyErrs {
-			t.Log(e)
-		}
-		t.Error()
-	}
-}
-
 func TestJSONState(t *testing.T) {
-	initState := []byte(`{
-		"community_id": "abc"
-	}`)
+	initState := community.NewCommunityStateBytes()
 	jsonState, err := NewJSONState(community.CommunityStateSchema, initState)
 	assert.Nil(t, err)
 
