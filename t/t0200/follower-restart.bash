ab9614405b06efe1ab4e2f7082b8bc3cf36778d1
Ethan Graf <graf.ethan@gmail.com>
2022-11-06 00:48:44 -0400

    Fix up tests

--- t/t0200/follower-restart.bash
+++ t/t0200/follower-restart.bash
@@ -45,42 +45,57 @@ docker-compose -f docker-compose-raft.yml up -V 2> /dev/null &
 atexit "docker-compose -f docker-compose-raft.yml down"
 atexit "docker-compose rm -f"
 
-echo $(wrapTransition "initialize_counter" $TRANSITION1)
-
 sleep 10 
 
-ALICE_NODE_ID=`$HABITATCTL_PATH -p 2000 community ls | head -n1 | awk '{print $3}'`
-BOB_NODE_ID=`$HABITATCTL_PATH -p 2001 community ls | head -n1 | awk '{print $3}'`
-CHARLIE_NODE_ID=`$HABITATCTL_PATH -p 2002 community ls | head -n1 | awk '{print $3}'`
+ID_PATH="$(temp::dir)"
+export HABITATCTL_IDENTITY_PATH=$ID_PATH
 
-COMMUNITY_UUID=`$HABITATCTL_PATH -p 2000 community create | head -n1 | awk '{print $NF}'`
+$HABITATCTL_PATH id init
+ALICE_NODE_CLI_CMD="$HABITATCTL_PATH -p 2000"
+BOB_NODE_CLI_CMD="$HABITATCTL_PATH -p 2001"
+CHARLIE_NODE_CLI_CMD="$HABITATCTL_PATH -p 2002"
+
+ALICE_NODE_ID=`$ALICE_NODE_CLI_CMD community ls | head -n1 | awk '{print $3}'`
+BOB_NODE_ID=`$BOB_NODE_CLI_CMD community ls | head -n1 | awk '{print $3}'`
+CHARLIE_NODE_ID=`$CHARLIE_NODE_CLI_CMD community ls | head -n1 | awk '{print $3}'`
+
+
+$ALICE_NODE_CLI_CMD id create --username alice --password abc
+$BOB_NODE_CLI_CMD id create --username bob --password abc
+$CHARLIE_NODE_CLI_CMD id create --username charlie --password abc
+
+ALICE_CMD="$ALICE_NODE_CLI_CMD --username alice --password abc"
+BOB_CMD="$BOB_NODE_CLI_CMD --username bob --password abc"
+CHARLIE_CMD="$CHARLIE_NODE_CLI_CMD --username charlie --password charlie"
+
+COMMUNITY_UUID=`$ALICE_CMD community create | head -n1 | awk '{print $1}'`
 
 sleep 2
 
-$HABITATCTL_PATH -p 2000 community propose -c $COMMUNITY_UUID $(wrapTransition "initialize_counter" $TRANSITION1)
+$ALICE_CMD community propose -c $COMMUNITY_UUID $(wrapTransition "initialize_counter" $TRANSITION1)
 
 ALICE_IP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' habitat_alice_1`
 BOB_IP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' habitat_bob_1`
 CHARLIE_IP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' habitat_charlie_1`
 
 sleep 1
-$HABITATCTL_PATH -p 2001 community join -c $COMMUNITY_UUID -a /ip4/$ALICE_IP/tcp/6000
-$HABITATCTL_PATH -p 2002 community join -c $COMMUNITY_UUID -a /ip4/$ALICE_IP/tcp/6000
+$BOB_CMD community join -c $COMMUNITY_UUID -a /ip4/$ALICE_IP/tcp/6000
+$CHARLIE_CMD community join -c $COMMUNITY_UUID -a /ip4/$ALICE_IP/tcp/6000
 sleep 3
-$HABITATCTL_PATH -p 2000 community add -c $COMMUNITY_UUID -n $BOB_NODE_ID -a /ip4/$BOB_IP/tcp/6000
-$HABITATCTL_PATH -p 2000 community add -c $COMMUNITY_UUID -n $CHARLIE_NODE_ID -a /ip4/$CHARLIE_IP/tcp/6000
+$ALICE_CMD community add -c $COMMUNITY_UUID -n $BOB_NODE_ID -a /ip4/$BOB_IP/tcp/6000
+$ALICE_CMD community add -c $COMMUNITY_UUID -n $CHARLIE_NODE_ID -a /ip4/$CHARLIE_IP/tcp/6000
 
 sleep 1
 
 docker restart habitat_bob_1
 
-$HABITATCTL_PATH -p 2000 community propose -c $COMMUNITY_UUID $(wrapTransition "increment_counter" $TRANSITION2)
+$ALICE_CMD community propose -c $COMMUNITY_UUID $(wrapTransition "increment_counter" $TRANSITION2)
 
 sleep 6
 
-COUNTER1=`$HABITATCTL_PATH -p 2000 community state -c $COMMUNITY_UUID | jq .counter`
-COUNTER2=`$HABITATCTL_PATH -p 2001 community state -c $COMMUNITY_UUID | jq .counter`
-COUNTER3=`$HABITATCTL_PATH -p 2002 community state -c $COMMUNITY_UUID | jq .counter`
+COUNTER1=`$ALICE_CMD community state -c $COMMUNITY_UUID | jq .counter`
+COUNTER2=`$BOB_CMD community state -c $COMMUNITY_UUID | jq .counter`
+COUNTER3=`$CHARLIE_CMD community state -c $COMMUNITY_UUID | jq .counter`
 
 sleep 1
 
