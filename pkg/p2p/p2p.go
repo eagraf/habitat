d75800419f92e62452f77fc46445ecca2d207e6e
Ethan Graf <graf.ethan@gmail.com>
2022-11-09 21:48:38 -0500

    pkg/p2p provides a way to make HTTP requests over libp2p, using a random temporary client

--- pkg/p2p/p2p.go
+++ pkg/p2p/p2p.go
@@ -2,14 +2,20 @@ package p2p
 
 import (
 	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
 
 	"github.com/eagraf/habitat/pkg/compass"
 	libp2p "github.com/libp2p/go-libp2p"
+	p2phttp "github.com/libp2p/go-libp2p-http"
 	"github.com/libp2p/go-libp2p/config"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
 	ma "github.com/multiformats/go-multiaddr"
-	"github.com/rs/zerolog/log"
 )
 
 type Node struct {
@@ -52,4 +58,44 @@ func standardHostOpts(priv crypto.PrivKey, listenAddrs []ma.Multiaddr) []config.
 		//	libp2p.EnableAutoRelay(),
 	}
 
+}
+
+func LibP2PHTTPRequestWithRandomClient(addr ma.Multiaddr, route string, peerID peer.ID, req *http.Request) (*http.Response, error) {
+	// generate a temporary host to make the request
+	ip, err := compass.LocalIPv4()
+	if err != nil {
+		return nil, err
+	}
+
+	// use a random port number - yuck!
+	port := strconv.Itoa(5000 + rand.Intn(5000))
+
+	privKey, _, err := crypto.GenerateKeyPair(crypto.Ed25519, 256)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO @eagraf handle IPv6
+	listen, _ := ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%s", ip, port))
+
+	fmt.Println(addr.String())
+
+	hostOpts := standardHostOpts(privKey, []ma.Multiaddr{listen})
+	h, _ := libp2p.New(hostOpts...)
+
+	h.Peerstore().AddAddr(peerID, addr, peerstore.PermanentAddrTTL)
+
+	tr := &http.Transport{}
+	tr.RegisterProtocol("libp2p", p2phttp.NewTransport(h))
+
+	client := &http.Client{
+		Transport: tr,
+	}
+	url, err := url.Parse(fmt.Sprintf("libp2p://%s%s", peerID.String(), route))
+	if err != nil {
+		return nil, err
+	}
+	req.URL = url
+
+	return client.Do(req)
 }
