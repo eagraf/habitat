fbef0c30d698f03eeaf5a47ea68b4e5d63380b67
Ethan Graf <graf.ethan@gmail.com>
2022-11-17 13:28:48 -0500

    PublicRaftMultiaddr helper distinguishes when its inside a docker container
    Factor out DecomposeNodeMultiaddr helper

--- pkg/compass/compass.go
+++ pkg/compass/compass.go
@@ -11,7 +11,9 @@ import (
 	"runtime"
 
 	"github.com/google/uuid"
+	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 const (
@@ -153,10 +155,21 @@ func PublicIP() (net.IP, error) {
 }
 
 func PublicRaftMultiaddr() (multiaddr.Multiaddr, error) {
-	ip, err := PublicIP()
-	if err != nil {
-		return nil, err
+	var ip net.IP
+	if InDockerContainer() {
+		localIP, err := LocalIPv4()
+		if err != nil {
+			return nil, err
+		}
+		ip = localIP
+	} else {
+		pubIP, err := PublicIP()
+		if err != nil {
+			return nil, err
+		}
+		ip = pubIP
 	}
+
 	ipVersion := "ip4"
 	if ip.To4() == nil {
 		ipVersion = "ip6"
@@ -168,7 +181,53 @@ func PublicRaftMultiaddr() (multiaddr.Multiaddr, error) {
 	return addr, nil
 }
 
+// TODO this should really be something like InTestingDockerContainer
+// You'd need a way to know that however
 func InDockerContainer() bool {
 	_, err := os.Stat("/.dockerenv")
 	return err == nil
+}
+
+func DecomposeNodeMultiaddr(multiaddr string) (peer.ID, ma.Multiaddr, error) {
+	remoteMA, err := ma.NewMultiaddr(multiaddr)
+	if err != nil {
+		return "", nil, err
+	}
+
+	b58PeerID, err := remoteMA.ValueForProtocol(ma.P_P2P)
+	if err != nil {
+		return "", nil, fmt.Errorf("couldn't retrieve p2p value in multiaddr: %s", multiaddr)
+	}
+
+	addrStr := ""
+	ip, err := remoteMA.ValueForProtocol(ma.P_IP4)
+	if err != nil {
+		ip, err = remoteMA.ValueForProtocol(ma.P_IP6)
+		if err != nil {
+			return "", nil, fmt.Errorf("couldn't retrieve ip4 or ip6 value in multiaddr: %s", multiaddr)
+		} else {
+			addrStr += "/ip6/" + ip
+		}
+	} else {
+		addrStr += "/ip4/" + ip
+	}
+
+	port, err := remoteMA.ValueForProtocol(ma.P_TCP)
+	if err != nil {
+		return "", nil, fmt.Errorf("couldn't retrieve tcp value in multiaddr: %s", multiaddr)
+	}
+	addrStr += "/tcp/" + port
+
+	addr, err := ma.NewMultiaddr(addrStr)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// decode base58 encoded peer id for setting addresses
+	peerID, err := peer.Decode(b58PeerID)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return peerID, addr, nil
 }
