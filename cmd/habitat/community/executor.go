d46bc2c0f023b857a858c30cd2aa411d96a3e923
Ethan Graf <graf.ethan@gmail.com>
2022-12-02 12:01:23 -0500

    Refactor executing state machine transitions a bit

--- cmd/habitat/community/executor.go
+++ cmd/habitat/community/executor.go
@@ -12,6 +12,11 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+type TransitionExecutor func(update *state.StateUpdate) error
+
+// CommunityExecutor implements the state.Executor interface.
+// It receives incoming state transitions from the replicated state machine,
+// and executes the commands on the running node.
 type CommunityExecutor struct {
 	processManager *procs.Manager
 }
@@ -22,33 +27,51 @@ func NewCommunityExecutor(p *procs.Manager) *CommunityExecutor {
 	}
 }
 
+// GetTransitionExecutor maps transition types to executor functions
+// TODO this is pretty silly, lets find a way to do this with reflection or equivalent
+func (e *CommunityExecutor) GetTransitionExecutor(transitionType string) TransitionExecutor {
+	switch transitionType {
+	case state.TransitionTypeInitializeIPFSSwarm:
+		return e.InitializeIPFSSwarm
+	default:
+		return nil
+	}
+}
+
 func (e *CommunityExecutor) Execute(update *state.StateUpdate) {
 	log.Info().Msgf("executing %s state transition", update.TransitionType)
+
+	transitionExecutor := e.GetTransitionExecutor(update.TransitionType)
+	if transitionExecutor != nil {
+		err := transitionExecutor(update)
+		if err != nil {
+			log.Error().Err(err).Msgf("error executing %s", update.TransitionType)
+		}
+	}
+}
+
+func (e *CommunityExecutor) InitializeIPFSSwarm(update *state.StateUpdate) error {
 	newState, err := update.State()
 	if err != nil {
-		log.Error().Msgf("error getting new state: %s", err)
+		return err
 	}
-	switch update.TransitionType {
-	case state.TransitionTypeInitializeIPFSSwarm:
-		communityIPFSConfig, err := json.Marshal(newState.IPFSConfig)
-		if err != nil {
-			log.Error().Msgf("error marshaling IPFS config")
-		}
-
-		communityIPFSConfigB64 := base64.StdEncoding.EncodeToString(communityIPFSConfig)
-		if err != nil {
-			log.Error().Msgf("error base64 encoding IPFS config")
-		}
-
-		ipfsPath := filepath.Join(compass.CommunitiesPath(), newState.CommunityID, "ipfs")
-		args := []string{ipfsPath}
-		flags := []string{"-c", communityIPFSConfigB64}
-
-		fmt.Println(newState.CommunityID)
-		procID, err := e.processManager.StartProcess("ipfs-driver", newState.CommunityID, args, []string{}, flags)
-		if err != nil {
-			log.Error().Msgf("error starting IPFS driver process")
-		}
-		fmt.Println(procID)
+	communityIPFSConfig, err := json.Marshal(newState.IPFSConfig)
+	if err != nil {
+		return fmt.Errorf("error marshaling IPFS config: %s", err)
 	}
+
+	communityIPFSConfigB64 := base64.StdEncoding.EncodeToString(communityIPFSConfig)
+	if err != nil {
+		return fmt.Errorf("error base64 encoding IPFS config: %s", err)
+	}
+
+	ipfsPath := filepath.Join(compass.CommunitiesPath(), newState.CommunityID, "ipfs")
+	args := []string{ipfsPath}
+	flags := []string{"-c", communityIPFSConfigB64}
+
+	_, err = e.processManager.StartProcess("ipfs-driver", newState.CommunityID, args, []string{}, flags)
+	if err != nil {
+		return fmt.Errorf("error starting IPFS driver process: %s", err)
+	}
+	return nil
 }
