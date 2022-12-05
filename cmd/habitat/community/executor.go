21a99c3211424f2cf21f31b90f6a9b0634aa1b08
Ethan Graf <graf.ethan@gmail.com>
2022-12-05 14:27:24 -0500

    Process manager starts apps as result of state changes

--- cmd/habitat/community/executor.go
+++ cmd/habitat/community/executor.go
@@ -1,14 +1,12 @@
 package community
 
 import (
-	"encoding/base64"
 	"encoding/json"
-	"fmt"
-	"path/filepath"
 
 	"github.com/eagraf/habitat/cmd/habitat/community/state"
 	"github.com/eagraf/habitat/cmd/habitat/node"
-	"github.com/eagraf/habitat/pkg/compass"
+	"github.com/eagraf/habitat/cmd/habitat/procs"
+	"github.com/eagraf/habitat/structs/community"
 	"github.com/rs/zerolog/log"
 )
 
@@ -33,6 +31,14 @@ func (e *CommunityExecutor) GetTransitionExecutor(transitionType string) Transit
 	switch transitionType {
 	case state.TransitionTypeInitializeIPFSSwarm:
 		return e.InitializeIPFSSwarm
+	case state.TransitionTypeStartProcess:
+		return e.StartProcess
+	case state.TransitionTypeStopProcess:
+		return e.StopProcess
+	case state.TransitionTypeStartProcessInstance:
+		return e.StartProcessInstance
+	case state.TransitionTypeStopProcessInstance:
+		return e.StartProcessInstance
 	default:
 		return nil
 	}
@@ -51,7 +57,7 @@ func (e *CommunityExecutor) Execute(update *state.StateUpdate) {
 }
 
 func (e *CommunityExecutor) InitializeIPFSSwarm(update *state.StateUpdate) error {
-	newState, err := update.State()
+	/*newState, err := update.State()
 	if err != nil {
 		return err
 	}
@@ -69,9 +75,82 @@ func (e *CommunityExecutor) InitializeIPFSSwarm(update *state.StateUpdate) error
 	args := []string{ipfsPath}
 	flags := []string{"-c", communityIPFSConfigB64}
 
-	_, err = e.node.ProcessManager.StartProcess("ipfs-driver", newState.CommunityID, args, []string{}, flags)
+	_, err = e.node.ProcessManager.StartProcessInstance("ipfs-driver", newState.CommunityID, args, []string{}, flags)
 	if err != nil {
 		return fmt.Errorf("error starting IPFS driver process: %s", err)
+	}*/
+	return nil
+}
+
+func (e *CommunityExecutor) StartProcess(update *state.StateUpdate) error {
+	return nil
+}
+
+func (e *CommunityExecutor) StopProcess(update *state.StateUpdate) error {
+	return nil
+}
+
+func (e *CommunityExecutor) StartProcessInstance(update *state.StateUpdate) error {
+
+	var transition state.StartProcessInstanceTransition
+	err := json.Unmarshal(update.Transition, &transition)
+	if err != nil {
+		return err
 	}
+
+	state, err := update.State()
+	if err != nil {
+		return err
+	}
+
+	var process *community.Process
+	for _, p := range state.Processes {
+		if p.ID == transition.ProcessInstance.ProcessID {
+			process = p
+		}
+	}
+
+	// Hash together the process ID, community ID and node ID to get a process instance ID
+
+	if transition.ProcessInstance.NodeID == e.node.ID {
+		_, err := e.node.ProcessManager.StartProcessInstance(state.CommunityID, process.ID, process.AppName, process.Args, process.Env, process.Flags)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (e *CommunityExecutor) StopProcessInstance(update *state.StateUpdate) error {
+	var transition state.StartProcessInstanceTransition
+	err := json.Unmarshal(update.Transition, &transition)
+	if err != nil {
+		return err
+	}
+
+	state, err := update.State()
+	if err != nil {
+		return err
+	}
+
+	var process *community.Process
+	for _, p := range state.Processes {
+		if p.ID == transition.ProcessInstance.ProcessID {
+			process = p
+		}
+	}
+
+	if transition.ProcessInstance.NodeID == e.node.ID {
+		processInstanceID, err := procs.GetProcessInstanceID(state.CommunityID, e.node.ID, process.ID)
+		if err != nil {
+			return err
+		}
+		err = e.node.ProcessManager.StopProcessInstance(processInstanceID)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
